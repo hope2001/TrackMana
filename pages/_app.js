dffa2689c65627cf5b7a8add41a1d3daf3851feb
Elisée OROUNLA <61190407+hope2001@users.noreply.github.com>
2023-01-13 17:26:50 +0100

    first deposit

---
+++ pages/_app.js
@@ -0,0 +1,22 @@
+// import '@/styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/assets/css/timetracker.style.min.css'
+import '../styles/assets/plugin/light-gallery/css/lightgallery.css'
+// import '../styles/assets/fonts/icofont/css/icofont.css'
+// import 'popper.js/dist/popper.min.js';
+import { useEffect } from 'react';
+// import 'bootstrap/dist/js/bootstrap.min.js'
+
+
+
+
+export default function App({ Component, pageProps }) {
+
+  useEffect(()=>{
+    // typeof document !== undefined ? require('bootstrap/dist/js/bootstrap.min.js'): null
+    // typeof document !== undefined ? require('popper.js/dist/popper.min.js'): null
+  })
+  
+  return <Component {...pageProps} />
+  
+}
